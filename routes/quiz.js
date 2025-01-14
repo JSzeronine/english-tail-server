92623b0b74a46be878a1fb9c76b6ecfe3836f7e2
Arden09 <kineage18@gmail.com>
2025-01-14 10:39:12 +0900

    My 랭킹

--- routes/quiz.js
+++ routes/quiz.js
@@ -21,6 +21,33 @@ router.get("/ranking", async (req, res) => {
     conn.release();
 });
 
+router.get( "/my-ranking", async ( req, res) => {
+    const conn = await db.getConnection();
+    const { userId } = req.query;
+
+    try{
+        const [result] = await conn.query(`
+            SELECT * FROM (
+                SELECT
+                tb2.name,
+                tb1.user_id,
+                tb1.score,
+                RANK() OVER (ORDER BY score DESC) AS ranking
+                FROM wt_score tb1
+                inner join wt_users tb2 on tb1.user_id = tb2.id
+            ) ranked_users
+            WHERE user_id = ?
+        `, [ userId ]);
+
+        res.status(200).json(result[0]);
+
+    }catch( err) {
+        console.error( err );
+    }
+
+    conn.release();
+})
+
 router.post("/finish", async (req, res) => {
     const conn = await db.getConnection();
     const {score, userId} = req.body.params;
